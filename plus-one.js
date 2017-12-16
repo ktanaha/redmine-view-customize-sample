9bf1f444bdb59611d2a692ea3698fe0f0e375f81
ktanaha <moutoku1984@gmail.com>
2017-12-16 16:50:20 +0900

    fixed plus one

---
+++ plus-one.js
@@ -0,0 +1,31 @@
+/*
+  Path patter : /issues/[0-9]+
+  Type        : JavaScript
+  
+  Add the value of the custom field of the specified numerical value.
+*/
+$(function() {
+  count_up_btn();
+  
+  $(document).ajaxComplete(function(){
+    count_up_btn();
+  });
+});
+
+function count_up_btn() {
+  var field = $('#issue_custom_field_values_1');
+  field.hide();
+
+  var btn = '<button type="button" class="add-btn">+1</button>';
+  field.after(btn);
+  
+  var count = '<span class="issue_custom_field_values_1_label">'+ field.val() +'</span>';
+  $('.add-btn').after(count)
+
+  $('.add-btn').on('click', function () {
+    var label = $('.issue_custom_field_values_1_label');
+    var num = Number(field.val()) + 1;
+    field.val(num)
+    label.text(num);
+  });
+}
