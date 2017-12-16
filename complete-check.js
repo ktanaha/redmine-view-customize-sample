7236a0a5097e055d973cd058ce693150481ca000
ktanaha <moutoku1984@gmail.com>
2017-12-16 10:04:31 +0900

    update complete-check.js

--- complete-check.js
+++ complete-check.js
@@ -4,6 +4,14 @@
   
   Perform input check when completing status
 */
+$(function() {
+    $('#issue_status_id').change(function() {
+        var status = $('#issue_status_id').val();
+        if (status === '5') {
+            $('#issue_custom_field_values_1').css('background-coloe','#FF0000');
+        }
+    });
+});
 
 $(function() {
     $('#issue-form input[type="submit"]').on('click', function(event) {
