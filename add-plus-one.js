3fecaa1e1741de009c8eca97e14b980343d9d427
ktanaha <moutoku1984@gmail.com>
2017-12-16 11:50:50 +0900

    change label

--- add-plus-one.js
+++ add-plus-one.js
@@ -5,20 +5,30 @@
   Add the value of the custom field of the specified numerical value.
 */
 $(function() {
+  change_field();
   add_btn();
-
+  
   $(document).ajaxComplete(function(){
+    change_field();
     add_btn();
   });
 });
 
 function add_btn() {
-  var label = $('#attributes > div:nth-child(2) > div.splitcontentleft > p > label');
+  var label = $('#issue_custom_field_values_1_label');
   var btn = '<button type="button" class="add-btn">+1</button>';
   label.append(btn);
 
   $('.add-btn').on('click', function () {
-    var field = $('#issue_custom_field_values_1');
+    var field = $('#issue_custom_field_values_1_label');
     field.val(Number(field.val()) + 1);
   });
+}
+
+function change_field() {
+  var field = $('#issue_custom_field_values_1');
+  field.hide();
+
+  var label = '<div id="issue_custom_field_values_1_label">'+ field.val() +'</div>';
+  field.append(label)
 }
