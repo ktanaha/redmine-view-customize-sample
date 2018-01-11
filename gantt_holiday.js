3a5af73b083fe36d374bc77391abef90242c5a5a
ktanaha <moutoku1984@gmail.com>
2018-01-11 22:22:18 +0900

    show holidays

---
+++ gantt_holiday.js
@@ -0,0 +1,14 @@
+/*
+  Path patter : /project/.+/easy_gantt
+  Type        : JavaScript
+  
+  show holidays
+*/
+window.ysy = { settings:
+    { holidays: 
+        [
+            // 祝日をYMD形式で指定する
+            "2017-01-01",
+        ]
+    }
+};
--- plus-one.js
+++ plus-one.js
@@ -5,27 +5,37 @@
   Add the value of the custom field of the specified numerical value.
 */
 $(function() {
-  count_up_btn();
+  count_up_down();
   
   $(document).ajaxComplete(function(){
-    count_up_btn();
+    count_up_down();
   });
 });
 
-function count_up_btn() {
+function count_up_down() {
   var field = $('#issue_custom_field_values_1');
   field.hide();
 
-  var btn = '<button type="button" class="count_up_btn">+1</button>';
-  field.after(btn);
+  var minus_btn = '<button type="button" class="count_down_btn">-1</button>';
+  field.after(minus_btn);
+
+  var plus_btn = '<button type="button" class="count_up_down">+1</button>';
+  $('.count_down_btn').after(plus_btn);
   
   var count = '<span class="issue_custom_field_values_1_label">'+ field.val() +'</span>';
-  $('.count_up_btn').after(count)
+  $('.count_up_down').after(count)
 
-  $('.count_up_btn').on('click', function () {
+  $('.count_up_down').on('click', function () {
     var label = $('.issue_custom_field_values_1_label');
     var num = Number(field.val()) + 1;
-    field.val(num)
+    field.val(num);
+    label.text(num);
+  });
+
+  $('.count_down_btn').on('click', function () {
+    var label = $('.issue_custom_field_values_1_label');
+    var num = Number(field.val()) - 1;
+    field.val(num);
     label.text(num);
   });
 }
